025859a87bdfbe911fd77b39659893283252a82d
Michael Morgan <morganmichael340@gmail.com>
2021-05-02 12:08:52 -0700

    search on keydown

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,6 +15,7 @@ class SearchBar extends React.Component {
     this.handleLocationChange = this.handleLocationChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
     this.handleSortByChange = this.handleSortByChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
 
     this.sortByOptions = {
       'Best Match': 'best_match',
@@ -48,6 +49,13 @@ class SearchBar extends React.Component {
     event.preventDefault();
   }
 
+  handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      this.props.searchYelp(this.state.term);
+      event.preventDefault();
+    }
+  }
+
   renderSortByOptions() {
     return Object.keys(this.sortByOptions).map(sortByOption => {
       let sortByOptionValue = this.sortByOptions[sortByOption];
@@ -68,7 +76,7 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Food, Hikes, Bars, etc.." onChange={this.handleTermChange} />
+          <input placeholder="Search Food, Hikes, Bars, etc.." onChange={this.handleTermChange} onKeyDown={this.handleKeyDown}/>
         </div>
         <div className="SearchBar-submit">
           <a onClick={this.handleSearch}>Let's Go</a>
